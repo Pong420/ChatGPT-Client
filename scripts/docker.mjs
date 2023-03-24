64575b12c33364dde3663ff3afdfeb80822ef28e
Pong <samfunghp@gmail.com>
2023-03-24 15:57:17 +0000

    fix(): docker script

--- scripts/docker.mjs
+++ scripts/docker.mjs
@@ -10,13 +10,15 @@ const { env } = await import('../src/env.mjs');
  * @returns
  */
 const spawn = commands => {
+  // print the command, this is helpful for create
+  console.log(`\`\`\`\n${commands.replace(/ --?\w+/g, s => ' \\\n ' + s)}\n\`\`\``);
   const [command = '', ...args] = commands.trim().split(' ');
   return _spawn(command, args);
 };
 
 const envArgs = Object.entries(env)
-  .filter(([k]) => k !== 'NODE_ENV')
-  .map((k, v) => `-e ${k}=${v}`);
+  .filter(([k]) => !['NODE_ENV', 'NEXTAUTH_URL'].includes(k))
+  .map(([k, v]) => `-e ${k}=${v}`);
 
 const [, , command] = process.argv;
 
@@ -26,6 +28,9 @@ switch (command) {
     break;
   case 'run':
     spawn(`docker run --name chatgpt -p 3000:3000 ${envArgs.join(' ')} -it chatgpt`);
+    break;
+  case 'remote':
+    spawn(`docker run --name chatgpt -p 3000:3000 ${envArgs.join(' ')} -it ghcr.io/pong420/chat-gpt-client:latest`);
     break;
   default:
     throw new Error('unknown command ${command}');
